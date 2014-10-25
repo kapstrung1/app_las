4b3cb1356763adeec063f6a49bc9905ae969edb1
kapstrung1 <andrew686@op.pl>
2014-10-25 06:41:20 +0200

    konfiguracja bazy danych

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,19 +47,23 @@ return array(
 			),
 		),
 		*/
+		// uncomment the following to use a sqlite database
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
-		// uncomment the following to use a MySQL database
-		/*
+		*/
+		// MySQL database
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=app_las',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
+			'tablePrefix' => 'app_',
 		),
-		*/
+	
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
